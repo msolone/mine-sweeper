8dfeda3f9a9304754c7050b65656133c0d92c2e7
Michael Solone <michaelsolone@gmail.com>
2018-08-20 21:48:25 -0400

    Updated CSS to included color-coded numbers

--- src/MineSweeper.js
+++ src/MineSweeper.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-
 const BASE_URL = "https://minesweeper-api.herokuapp.com/";
 
 class MineSweeper extends Component {
@@ -15,7 +14,7 @@ class MineSweeper extends Component {
     };
   }
 
-  createGame () {
+  createGame() {
     fetch(`${BASE_URL}games`, {
       method: "POST",
       body: JSON.stringify({ difficulty: this.state.difficulty }),
@@ -33,37 +32,37 @@ class MineSweeper extends Component {
   }
 
   boxClicked = (i, j) => {
-    if (this.state.game.state !== 'lost' && this.state.game.state !== 'won') { 
-    console.log("box clicked ", i, j);
-    fetch(`${BASE_URL}games/${this.state.game.id}/check`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ row: i, col: j })
-    })
-      .then(resp => resp.json())
-      .then(latestGameData => {
-        if (latestGameData.state === "lost") {
-          console.log(latestGameData);
-          this.setState({
-            game: latestGameData,
-            message: "You Lose"
-          });
-        } else if (latestGameData.state === "won") {
-          console.log(latestGameData);
-          this.setState({
-            game: latestGameData,
-            message: "You Win"
-          });
-        } else {
-          console.log(latestGameData);
-          this.setState({
-            game: latestGameData,
-            message: "Keep Playing"
-          });
-        }
-      });
+    if (this.state.game.state !== "lost" && this.state.game.state !== "won") {
+      console.log("box clicked ", i, j);
+      fetch(`${BASE_URL}games/${this.state.game.id}/check`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ row: i, col: j })
+      })
+        .then(resp => resp.json())
+        .then(latestGameData => {
+          if (latestGameData.state === "lost") {
+            console.log(latestGameData);
+            this.setState({
+              game: latestGameData,
+              message: "You Lose"
+            });
+          } else if (latestGameData.state === "won") {
+            console.log(latestGameData);
+            this.setState({
+              game: latestGameData,
+              message: "You Win"
+            });
+          } else {
+            console.log(latestGameData);
+            this.setState({
+              game: latestGameData,
+              message: "Keep Playing"
+            });
+          }
+        });
     }
   };
 
@@ -114,29 +113,58 @@ class MineSweeper extends Component {
   };
 
   renderSquares = (i, j) => {
-      console.log(this.state.game.board[i][j])
-      console.log(typeof this.state.game.board[i][j])
+    let letterClass = "";
     if (this.state.game.board[i][j] === "F") {
-      return <span role='img'>"🚩"</span>;
-    } else if (this.state.game.board[i][j] === "*") {
-      return <span role='img'>"💣"</span>;
-    } else if (this.state.game.board[i][j] === "_") {
-        return '_'  
-    } else if (this.state.game.board[i][j] === 1) {
-        return <span className='blue-num'>1</span>
-    } else if (this.state.game.board[i][j] === 2) {
-        return <span className='green-num'>2</span>} 
-    else {
-        return this.state.game.board[i][j]
+      letterClass = "flag";
     }
+    return (
+      <td
+        className={`squares ${letterClass}`}
+        key={j}
+        onClick={() => this.boxClicked(i, j)}
+        onContextMenu={event => this.boxRightClicked(event, i, j)}
+      >
+        {this.getSquareContent(i, j)}
+      </td>
+    );
   };
 
-  resetGame = () => {
-      this.setState({
-          message:'New Game'
-      }, this.createGame())
-    
+  getSquareContent(i, j) {
+    if (this.state.game.board[i][j] === "F") {
+      return <span role="img">🚩</span>;
+    } else if (this.state.game.board[i][j] === "*") {
+      return <span role="img">💣</span>;
+    } else if (this.state.game.board[i][j] === "_") {
+      return '_';
+    } else if (this.state.game.board[i][j] === 1) {
+      return <span className="blue-num">1</span>;
+    } else if (this.state.game.board[i][j] === 2) {
+      return <span className="green-num">2</span>;
+    } else if (this.state.game.board[i][j] === 3) {
+      return <span className="red-num">3</span>;
+    } else if (this.state.game.board[i][j] === 4) {
+        return <span className='purple-num'>4</span>
+    } else if (this.state.game.board[i][j] === 5) {
+      return <span className='black-num'>5</span>
+    } else if (this.state.game.board[i][j] === 6) {
+        return <span className='maroon-num'>6</span>
+    } else if (this.state.game.board[i][j] === 7) {
+        return <span className='gray-num'>7</span>
+    } else if (this.state.game.board[i][j] === 8) {
+        return <span className='turquoise-num'>8</span>  
+    } else {
+      return this.state.game.board[i][j];
+    }
   }
+
+  resetGame = () => {
+    this.setState(
+      {
+        message: "New Game"
+      },
+      this.createGame()
+    );
+  };
 
   render() {
     return (
@@ -147,7 +175,11 @@ class MineSweeper extends Component {
           <button onClick={this.mediumMode}>Medium</button>
           <button onClick={this.hardMode}>Hard</button>
         </div>
-        <button className='reset-button'><span role='img' onClick={this.resetGame}>😀</span></button>
+        <button className="reset-button">
+          <span role="img" onClick={this.resetGame}>
+            😀
+          </span>
+        </button>
         <h1 className="playing-message">{this.state.message}</h1>
         <table className="game-board">
           <tbody>
@@ -155,18 +187,7 @@ class MineSweeper extends Component {
               return (
                 <tr key={i} className="rows">
                   {row.map((col, j) => {
-                    return (
-                      <td
-                        className="squares"
-                        key={j}
-                        onClick={() => this.boxClicked(i, j)}
-                        onContextMenu={event =>
-                          this.boxRightClicked(event, i, j)
-                        }
-                      >
-                        {this.renderSquares(i, j)}
-                      </td>
-                    );
+                    return this.renderSquares(i, j);
                   })}
                 </tr>
               );
